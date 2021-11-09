eab2307bbb5a78d575ecbec157cef952ac4716bf
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-11-08 23:03:36 -0500

    all new files

---
+++ bar-chart-project-instructions/common.js
@@ -0,0 +1,29 @@
+// var basic = drawBarChart([
+//   {
+//     name: 'One',
+//     value: 10,
+//   },
+//   {
+//     name: 'Two',
+//     value: 20,
+//   },
+//   {
+//     name: 'Three',
+//     value: 30,
+//   },
+// {
+//     name: 'Four',
+//     value: 15,
+//   },
+// ]), {
+//   title: 'Basic Barchart',
+//   gridlineSpacingY: 5,
+// }, 'barchart-basic');
+
+// eval(basic);
+// $("#barchart-basic-code").text(basic);
+
+console.log('hello world')
+
+const matrix = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+console.log(matrix)
